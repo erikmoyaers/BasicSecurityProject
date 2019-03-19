320dfc797b4a47dc519639a340cdbab521ea5548
IlyasMercan <ilyas.mercan@skynet.be>
2019-03-19 21:31:38 +0100

    Added webpages and registration functionality

--- BasicSecurityProject/BasicSecurityProject/Controllers/AccountController.cs
+++ BasicSecurityProject/BasicSecurityProject/Controllers/AccountController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using BasicSecurityProject.Models;
 using BasicSecurityProject.Services;
+using BasicSecurityProject.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BasicSecurityProject.Controllers
@@ -11,26 +14,95 @@ namespace BasicSecurityProject.Controllers
     public class AccountController : Controller
     {
         private readonly IAccountRepository _accountRepository;
-        
+        private readonly SHA256 _sha256 = SHA256.Create();
 
         public AccountController(IAccountRepository accountRepository)
         {
             _accountRepository = accountRepository;
-            
+        }
+
+        public IActionResult Index()
+        {
+            return View();
         }
 
         [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        //PROBLEEM: user komt niet in de databank 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(AccountViewModel account)
+        {
+            if(ModelState.IsValid)
+            {
+                if(account.Password.Equals(account.PasswordAgain))
+                {
+                    var newAccount = new Account();
+                    newAccount.Username = account.Username;
+                    newAccount.Salt = getSalt();
+                    //van wat ik kan verstaan: wordt gewoon achter elkaar bijgevoegd. klopt dit?
+                    newAccount.Hash = Encoding.UTF8.GetString(_sha256.ComputeHash(Encoding.ASCII.GetBytes(account.Password + newAccount.Salt)));
+                    _accountRepository.CreateAccount(newAccount);
+                    return RedirectToAction(nameof(EncryptOrDecryptChoice), new { id = newAccount.ID });//potentiele fout: hoe weet die het ID ?
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public IActionResult EncryptOrDecryptChoice(int id)
+        {
+            var model = _accountRepository.FindById(id);
+            if(model == null)
+            {
+                return View("Not found"); //mss beter een of andere fancy niet gevonden pagina
+            }
+            return View(model);
+        }
+
+        //https://codereview.stackexchange.com/questions/93614/salt-generation-in-c
+        //STEEKT MOMENTEEL GEWOON HIER. NIEUWE KLASSE MAKEN ?
+        public static string getSalt()
+        {
+            var random = new RNGCryptoServiceProvider();
+
+            // Maximum length of salt
+            int max_length = 32;
+
+            // Empty salt array
+            byte[] salt = new byte[max_length];
+
+            // Build the random bytes
+            random.GetNonZeroBytes(salt);
+
+            // Return the string encoded salt
+            return Convert.ToBase64String(salt);
+        }
+
+        /*
         public IActionResult Index()
         {
             var model = _accountRepository.GetAll();
             return View(model);
         }
+
         [HttpPost]
         public IActionResult View()
         {
             
             return View();
         }
+        */
 
     }
 }
---
+++ BasicSecurityProject/BasicSecurityProject/ViewModel/AccountViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicSecurityProject.ViewModel
+{
+    public class AccountViewModel
+    {
+        //zet hier logica omtrent lengte bij. in databank staat er VARCHAR(MAX) maar test dit eens de applicatie bolt
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string PasswordAgain { get; set; }
+    }
+}
